2ee6e2fd37b9f8753d504dcd6706d65f6466dae4
Sajad pp <96676224+sajadpp120@users.noreply.github.com>
2022-02-03 13:56:50 +0530

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -14,61 +14,61 @@ let allMusic = [
     name: "Harley Bird - Home",
     artist: "Jordan Schor",
     img: "music-1-min",
-    src: "music-1-min"
+    src: "music-1"
   },
   {
     name: "Ikson Anywhere – Ikson",
     artist: "Audio Library",
     img: "music-2-min",
-    src: "music-2-min"
+    src: "music-2"
   },
   {
     name: "Beauz & Jvna - Crazy",
     artist: "Beauz & Jvna",
     img: "music-3-min",
-    src: "music-3-min"
+    src: "music-3"
   },
   {
     name: "Hardwind - Want Me",
     artist: "Mike Archangelo",
     img: "music-4-min",
-    src: "music-4-min"
+    src: "music-4"
   },
   {
     name: "Jim - Sun Goes Down",
     artist: "Jim Yosef x Roy",
     img: "music-5-min",
-    src: "music-5-min"
+    src: "music-5"
   },
   {
     name: "Lost Sky - Vision NCS",
     artist: "NCS Release",
     img: "music-6-min",
-    src: "music-6-min"
+    src: "music-6"
   },
   {
     name: "Manikya chirakulla - Iduki gold",
     artist: "Job kurien, Bijibal, Rafeeq Ahammed",
     img: "music-7-min",
-    src: "music-7-min"
+    src: "music-7"
   },
   {
     name: "Changes - Xxxtentaction",
     artist: "Xxxtentaction",
     img: "music-8-min",
-    src: "music-8-min"
+    src: "music-8"
   },
   {
     name: "Sickick - Intro(Infected)",
     artist: "Sickick",
     img: "music-9-min",
-    src: "music-9-min"
+    src: "music-9"
   },
   {
     name: "Pushpa - Shrivalli(Malayalam)",
     artist: "Sid sriram",
     img: "music-10-min",
-    src: "music-10-min",
+    src: "music-10",
   },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
